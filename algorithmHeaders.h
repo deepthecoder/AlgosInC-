59c9aa1c6841ba47b83a1b002c3b5c60a9ff67bf
Shubharthi Dey <deysubharthi15@gmail.com>
2021-02-11 22:46:48 +0530

    Create algorithmHeaders.h
    Added the header file(includes all necessary function signature)

---
+++ algorithmHeaders.h
@@ -0,0 +1,16 @@
+#include<iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+
+vector<int> generateVector(int,int,int);
+void printVector(vector<int>);
+void selectionSort(vector<int> &L);
+void rselectionSort(vector<int> &L,int);
+void bubbleSort(vector<int> &L);
+void rbubbleSort(vector<int> &L,int);
+void insertionSort(vector<int> &L,int);
+void rinsertionSort(vector<int> &L, int);
+
+int binarySearch(vector<int> &L, int key);
+int linearSearch(vector<int> &L, int key);
