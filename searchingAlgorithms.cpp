a5d26bb69ad185284d82b8fd6105cca3e4aab547
Shubharthi Dey <deysubharthi15@gmail.com>
2021-02-12 02:06:46 +0530

    Updated the commands for the searchAlgorithms file

--- searchingAlgorithms.cpp
+++ searchingAlgorithms.cpp
@@ -1,9 +1,22 @@
 #include "algorithmHeaders.h"
 
+/*
+   algorithm : binary search
+   params    : vector of integers, key 
+   assumption: Vector is already sorted
+   return    : index of the searched item in the vector
+*/
 int binarySearch(vector<int> &L,int key)
 {
    int index=-1;
    int low=0,high=L.size()-1;
+   
+   /*
+      Iterates and compares the middle element of
+      vector with the key element.
+      The middle element is calculated as 
+      mid=(low+high)/2
+   */
    while(low<=high)
    {
        int mid=(low+high)/2;
@@ -17,8 +30,20 @@ int binarySearch(vector<int> &L,int key)
    return index;
 }
 
+/*
+   algorithm : linear search
+   params    : vector of integers, key 
+   assumption: length of vector>=1
+   return    : index of the searched item in the vector
+*/
+
 int linearSearch(vector<int> &L,int key)
 {
+    /*
+      Algorithm does a linear scan of the sequence of 
+      elements and once found returns the index of
+      the key.
+    */
     int lengthOfVector=L.size();
     int index=-1;
     for(int i=0;i<lengthOfVector;i++)
