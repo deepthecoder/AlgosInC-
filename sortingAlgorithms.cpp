19f3bbbc0e8a3bf119b32e3fd52e7a2f154fb7b3
Shubharthi Dey <deysubharthi15@gmail.com>
2021-02-12 02:35:20 +0530

    Updated the sortedAlgorithm file with comments

--- sortingAlgorithms.cpp
+++ sortingAlgorithms.cpp
@@ -1,7 +1,24 @@
 #include "algorithmHeaders.h"
-
+/*
+    All the sorting functions are assumed to 
+    sort the vector of integers in the ascending
+    order.
+*/
+/*
+    Below are the two helper functions
+    which sort the given vector of integers
+    both iteratively and recursively.
+    Idea : Sorts the vector of integers 
+           by repeatedly finding the minimum element.
+*/
 void selectionSort(vector<int> &L)
 {
+    /*
+        variable description:
+        minIndex: variable tries to find the index
+                  which points to the minimum value 
+                  from the unsorted vector.
+    */
     int i,j,minIndex;
     int length=L.size();
     for(i=0;i<length-1;i++)
@@ -18,7 +35,7 @@ void selectionSort(vector<int> &L)
 
 void rselectionSort(vector<int> &L, int startIndex)
 {
-    if ( startIndex >= L.size()-1 )
+    if ( startIndex >= L.size()-1 )      //Base Condition for the recursion
         return;
     int minIndex = startIndex;
     for ( int index = startIndex + 1; index < L.size(); index++ )
@@ -30,7 +47,10 @@ void rselectionSort(vector<int> &L, int startIndex)
     rselectionSort(L, startIndex + 1);
 }
 
-
+/*
+    Bubble Sort algorithm swaps the adjacent elements in a vector
+    and performs the same repeatedly until the entire vector is sorted.
+*/
 void bubbleSort(vector<int> &L)
 {
     int length=L.size();
@@ -46,7 +66,7 @@ void bubbleSort(vector<int> &L)
 
 void rbubbleSort(vector<int> &L,int n)
 {
-    if(n==1)
+    if(n==1)            //Base Case to avoid Stack overflow
         return;
     for(int i=0;i<n-1;i++)
     {
@@ -64,6 +84,9 @@ void insertionSort(vector<int> &L, int n)
     {
         key=L.at(i);
         int j=i-1;
+        /* Move elements of L[0..i-1], that are  
+        greater than key, to one position ahead  
+        of their current position */
         while(j>=0 && L.at(j)>key)
         {
             L[j+1]=L[j];
